cbe80be49aef0bf37379dfb5b0bdd21dd01f5851
Antoine Edy <antoineedy@outlook.fr>
2024-05-21 23:27:00 +0100

    witched from HF to local because of limit in API

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, render_template
 import requests
+from transformers import pipeline
+
+pipe = pipeline(
+    "token-classification", model="antoineedy/stanford-deidentifier-base-finetuned-ner"
+)
 
 app = Flask(__name__)
 
@@ -8,28 +13,53 @@ HUGGING_FACE_API_URL = "https://api-inference.huggingface.co/models/antoineedy/s
 HUGGING_FACE_API_KEY = "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"
 
 
+def postprocess_results(result, input):
+    if result == "Error: Could not process the request.":
+        return "Error"
+    to_return = ""
+    for entity in result:
+        label = entity["entity"].split("_")[1]
+        # word = entity["word"]
+        start, end = entity["start"], entity["end"]
+        word = input[start:end]
+        if len(word) == 1:
+            score = ""
+            space = ""
+        else:
+            score = str(round(100 * entity["score"], 2)) + "%"
+            space = " "
+        to_return += (
+            f'<span class="highlight label-{label}">{word}<span class="score">{score}</span></span>'
+            + space
+        )
+    return to_return
+
+
+def mypipeline(user_input):
+    out = pipe(user_input)
+    to_remove = []
+    for i, entity in enumerate(out):
+        if entity["word"][:2] == "##":
+            out[i - 1]["end"] = entity["end"]
+            out[i - 1]["word"] += entity["word"][2:]
+            to_remove.append(i)
+    for i in sorted(to_remove, reverse=True):
+        out.pop(i)
+    return out
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     result = None
     to_return = None
     if request.method == "POST":
         user_input = request.form["user_input"]
-        headers = {"Authorization": f"Bearer {HUGGING_FACE_API_KEY}"}
-        response = requests.post(
-            HUGGING_FACE_API_URL, headers=headers, json={"inputs": user_input}
-        )
-        if response.status_code == 200:
-            result = response.json()
-        else:
-            result = "Error: Could not process the request."
-        to_return = ""
-        for entity in result:
-            if entity["entity_group"] == "LABEL_0":
-                to_return += f'<span style="color: red;">{entity["word"]}</span> '
-            elif entity["entity_group"] == "LABEL_1":
-                to_return += f'<span style="color: blue;">{entity["word"]}</span> '
-            else:
-                to_return += f'<span style="color: green;">{entity["word"]}</span> '
+        # headers = {"Authorization": f"Bearer {HUGGING_FACE_API_KEY}"}
+        # response = requests.post(
+        #    HUGGING_FACE_API_URL, headers=headers, json={"inputs": user_input}
+        # )
+        result = mypipeline(user_input)
+        to_return = postprocess_results(result, user_input)
 
     return render_template("index.html", result=to_return)
 
